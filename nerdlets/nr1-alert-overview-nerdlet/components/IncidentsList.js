e89602b9a6a1ca08ed21debde10b4a6cf13a4a28
Pavan Kumar C K <pkumar@newrelic.com>
2019-11-08 11:36:11 -0500

    updating chart with account facet

--- nerdlets/nr1-alert-overview-nerdlet/components/IncidentsList.js
+++ nerdlets/nr1-alert-overview-nerdlet/components/IncidentsList.js
@@ -26,8 +26,12 @@ export default class IncidentsList extends React.Component {
     const statusClosed = "'closed'";
     const alertTrend = 'SELECT count(*) FROM ' + this.eventName + ' timeseries Since ' + this.days + ' days ago facet  current_state';
     const alertCountByAccount = 'SELECT count(*) FROM ' + this.eventName + '  where current_state=' + statusOpen + ' FACET account_name Since ' + this.days + ' days ago';
+    const alertCountByCondition = 'SELECT count(*)  FROM ' + this.eventName + '  WHERE current_state = ' + statusOpen + ' SINCE ' + this.days + ' days ago FACET condition_name';
+    const averageDurationByCondition = 'SELECT average(duration)/1000/60  FROM ' + this.eventName + '  WHERE current_state = ' + statusClosed + ' SINCE ' + this.days + ' days ago FACET condition_name';
+    const alertCountByPolicy = 'SELECT count(*)  FROM ' + this.eventName + '  WHERE current_state = ' + statusOpen + ' SINCE ' + this.days + ' days ago FACET policy_name';
     const averageDurationByPolicy = 'SELECT average(duration)/1000/60  FROM ' + this.eventName + '  WHERE current_state = ' + statusClosed + ' SINCE ' + this.days + ' days ago FACET policy_name';
     const alertCountWOW = 'SELECT count(*) FROM ' + this.eventName + '  WHERE current_state = ' + statusOpen + ' SINCE 1 week ago COMPARE WITH 1 week ago';
+    
     const final = {}
 
     this.props.incidentsList.map(incident => {
@@ -154,6 +158,13 @@ export default class IncidentsList extends React.Component {
           />
         </div>
         <Stack horizontalType={Stack.HORIZONTAL_TYPE.FILL_EVENLY} fullWidth>
+        <StackItem grow>
+            <h3>Alert Count By Account - {this.days} day</h3>
+            <PieChart fullWidth
+              accountId={this.accountId}
+              query={alertCountByAccount}
+            />
+          </StackItem>
           <StackItem grow>
             <h3>Incident Count (Compared With Last Week)</h3>
             <BillboardChart fullWidth
@@ -168,22 +179,44 @@ export default class IncidentsList extends React.Component {
               query={alertTrend}
             />
           </StackItem>
+          <StackItem grow>
+            <h3>Incident Count (Compared With Last Week)</h3>
+            <BillboardChart fullWidth
+              accountId={this.accountId}
+              query={alertCountWOW}
+            />
+          </StackItem>
         </Stack>
         <Stack horizontalType={Stack.HORIZONTAL_TYPE.FILL_EVENLY} fullWidth>
           <StackItem grow>
-            <h3>Alert Count By Account - {this.days} day</h3>
+            <h3>Alert Count By Poilcy - {this.days} day</h3>
             <PieChart fullWidth
               accountId={this.accountId}
-              query={alertCountByAccount}
+              query={alertCountByPolicy}
             />
           </StackItem>
           <StackItem grow>
-            <h3>Average Incident Duration By Policy - {this.days} day</h3>
+            <h3>Average Incident Duration By Poilcy - {this.days} day</h3>
             <BarChart fullWidth
               accountId={this.accountId}
               query={averageDurationByPolicy}
             />
           </StackItem>
+          <StackItem grow>
+            <h3>Alert Count By Condition - {this.days} day</h3>
+            <PieChart fullWidth
+              accountId={this.accountId}
+              query={alertCountByCondition}
+            />
+          </StackItem>
+          <StackItem grow>
+            <h3>Average Incident Duration By Condition  - {this.days} day</h3>
+            <BarChart fullWidth
+              accountId={this.accountId}
+              query={averageDurationByCondition}
+            />
+          </StackItem>
+          
         </Stack>
       </div>
     )
